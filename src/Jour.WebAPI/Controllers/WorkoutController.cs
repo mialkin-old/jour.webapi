5863990f0a9e578530ff1f49e1526b8a4b10309f
Aleksei <anmyalkin@gmail.com>
2021-04-03 00:55:39 +0300

    Adds workout controller

---
+++ src/Jour.WebAPI/Controllers/WorkoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jour.WebAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class WorkoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok(new { key = "Hello from Jour.WebAPI" });
+        }
+    }
+}
