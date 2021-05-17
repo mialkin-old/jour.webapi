8a5818d9e430f26814b05596a357dadbfe981033
Aleksei Mialkin <anmyalkin@gmail.com>
2021-05-17 20:04:52 +0300

    Applies migration

---
+++ src/Jour.Database.Migrations/Migrations/20210517162934_13.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jour.Database.Migrations.Migrations
+{
+    public partial class _13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "workout_date",
+                table: "workouts",
+                newName: "workout_date_utc");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "workout_date_utc",
+                table: "workouts",
+                newName: "workout_date");
+        }
+    }
+}
